031a30bb6ed585d5e63dce11bb9afbb9a7b6129d
Christopher <chrisblupodev@gmail.com>
2016-05-23 13:34:28 +0000

    add radio buttons. and links to the elements

--- script.js
+++ script.js
@@ -21,28 +21,9 @@ var myApp = angular.module('myApp', ['ngMaterial']);
 myApp.controller('mainController', ['$scope', '$http', function($scope, $http) {
 
     $scope.twitchInfo = []; // store info for twitch users
-    $scope.names = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff", "ogamingsc2", "sheevergaming", "cretetion"];
-
-    // todo try and put this in the html
-    $scope.captureType = function(e) {
-        $scope.type = e.srcElement.name.toLowerCase();
-
-        // filter based on type
-    }
+    $scope.names = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff", "ogamingsc2", "sheevergaming", "cretetion", "c9sneaky", "domingo", "starladder5", "amazhs", "mlg", "mccool12345678", "skuart_tv", "dimmerhook", "girlsluvlps"];
 
     function asyncCollect() {
-        // function updateOfflineAndOnline() {
-        //     $scope.twitchInfoOffline = {};
-        //     $scope.twitchInfoOnline = {};
-        //     for (var key in $scope.twitchInfo) {
-        //         if ($scope.twitchInfo[key].isStreaming) {
-        //             $scope.twitchInfoOnline[key] = $scope.twitchInfo[key];
-        //         }
-        //         else {
-        //             $scope.twitchInfoOffline[key] = $scope.twitchInfo[key];
-        //         }
-        //     }
-        // }
 
         $scope.names.forEach(function(val, idx) {
 
@@ -68,7 +49,7 @@ myApp.controller('mainController', ['$scope', '$http', function($scope, $http) {
                 .success(function(data) {
                     current.logo = data.logo;
                     current.displayName = data.display_name;
-                    l(current.logo);
+                    current.url = "https://www.twitch.tv/" + data.display_name;
                     if (current.logo !== null) {
                         $scope.twitchInfo.push(current);
                     }
@@ -77,51 +58,12 @@ myApp.controller('mainController', ['$scope', '$http', function($scope, $http) {
                     l(err);
                 })
 
-            // get streaming info
-            // $.ajax({
-            //     url: 'https://api.twitch.tv/kraken/streams/{0}'.lp_format(val),
-            //     dataType: 'jsonp',
-            //     success: function(data) {
-            //         if (!! data.stream) {
-            //             $.extend($scope.twitchInfo[val], {
-            //                 // profilePic: data.stream.preview.small,
-            //                 isStreaming: true,
-            //                 streamName: data.stream.game
-            //             });
-            //         }
-            //         updateOfflineAndOnline();
-            //         $scope.$apply();
-            //     }
-            // });
-
-            // get logo and display name
-            // $.ajax({
-            //     url: 'https://api.twitch.tv/kraken/channels/{0}'.lp_format(val),
-            //     dataType: 'jsonp',
-            //     success: function (data) {
-            //         $.extend($scope.twitchInfo[val], {
-            //             logo: data.logo,
-            //             displayName: data.display_name
-            //         });
-            //         updateOfflineAndOnline();
-            //         $scope.$apply();
-            //     }
-            // });
         });
     }
     asyncCollect();
 
-    // $scope.names = ["admiral_bahroo", "freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","comster404","brunofin","thomasballinger","noobs2ninjas","beohoff"];
-    // $scope.twitchInfo = {};
-    // $scope.twitchInfoOnline = {};
-    // $scope.twitchInfoOffline = {};
-    // $scope.names.forEach(function(val, idx) {
-    //     $scope.twitchInfo[val] = {};
-    // });
-
     // create array for the info so ngreapeat can traverse it
     $scope.gettwitchInfo = function(type) {
-        l(type);
         if (type === 'all') {
             return $.map($scope.twitchInfo, function(elem, idx) {
                 return [elem];
@@ -138,16 +80,4 @@ myApp.controller('mainController', ['$scope', '$http', function($scope, $http) {
             });
         }
     }
-
-    // asyncCollect();
-
-    // $('button').on('click', function(e) {
-    //     var listName = 'ul[name="{0}"]'.lp_format($(e.target).text().toLowerCase());
-    //     if (jQuery(listName).hasClass('hide')) {
-    //         $('ul').removeClass('show');
-    //         $('ul').addClass('hide');
-    //         $(listName).addClass('show');
-    //         $(listName).removeClass('hide');
-    //     }
-    // });
 }]);
